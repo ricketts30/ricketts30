7da4ca62510d549624abe7be68b2a70cf1efd83b
ricketts30@yahoo.com <ricketts30@yahoo.com>
2017-04-15 20:06:19 +0100

    added boostrap capability

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
+
+
 import React, { Component } from 'react';
+import { Navbar, Jumbotron, Button, Grid } from 'react-bootstrap';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">Home</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
+        <Jumbotron>
+          <Grid>
+            <h1>Welcome to Rickett30</h1>
+            <p>A playground of React.js experiments...</p>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://react-bootstrap.github.io/components.html"
+                target="_blank">
+                View React Bootstrap Docs
+              </Button>
+            </p>
+            <p><img src={logo} className="App-logo" alt="logo" /></p>
+          </Grid>
+        </Jumbotron>
       </div>
     );
   }
