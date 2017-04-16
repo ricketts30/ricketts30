1efa0fa30a34cc0615e0020e6be8e6ff6a48c16f
ricketts30@yahoo.com <ricketts30@yahoo.com>
2017-04-16 23:26:07 +0100

    added some nicer form controls to the plot page

--- src/PagePlots.js
+++ src/PagePlots.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Grid, Button } from 'react-bootstrap';
+import { Grid, Button, FormGroup, FormControl, ControlLabel, Col, Form } from 'react-bootstrap';
 
 import WidgetMenu from './WidgetMenu'
 
@@ -15,6 +15,12 @@ class PagePlots extends Component {
         super(props);
         this.state = {
             qString: props.qString,
+            protagonist: "Theresa May",
+            partner: "Boris Johnson",
+            peril: "Brexit",
+            place: "The EU Parliament",
+            passion: "a love of being overly smug",
+            power: "President Trump"
         };
     }
 
@@ -24,12 +30,33 @@ class PagePlots extends Component {
         <WidgetMenu qString={this.state.qString} />
           <Grid className="PageTop">
             <h1>Plots</h1>
-            <p>protagonist: </p>
-            <p>place: </p>
-            <p>threat: </p>
-            <p>focus: </p>
-            <p>partner: </p>
-            <p>power: </p>
+            
+            <Form horizontal>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>Protagonist</Col>
+                  <Col sm={10}><FormControl type="text" placeholder="Protagonist" value={this.state.protagonist} /></Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>Partner</Col>
+                  <Col sm={10}><FormControl type="text" placeholder="Partner" value={this.state.partner} /></Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>Place</Col>
+                  <Col sm={10}><FormControl type="text" placeholder="Place" value={this.state.place} /></Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>Peril</Col>
+                  <Col sm={10}><FormControl type="text" placeholder="Peril" value={this.state.peril}/></Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>Passion</Col>
+                  <Col sm={10}><FormControl type="text" placeholder="Passion" value={this.state.passion} /></Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>Power</Col>
+                  <Col sm={10}><FormControl type="text" placeholder="Power" value={this.state.power} /></Col>
+                </FormGroup>
+            </Form>
             <p>
               <Button
                 bsStyle="success"
